b073887faaec2b07fc0b3da48a9ea45034a0c761
Swdenis <swdenis@mail.ru>
2021-01-19 11:11:05 +0100

    * .gitignore: Initial commit
    * PartyInvites.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * site.js:
    * PartyInvites.csproj:
    * favicon.ico:
    * site.css:
    * GuestResponse.cs:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * Error.cshtml:
    * Privacy.cshtml:
    * RsvpForm.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.min.css:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning!" : "Good afternoon!";
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
